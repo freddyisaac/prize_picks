2cddd1aec69c905c5df71947da08715dc92b02db
Frederick Isaac <freddyisaac@yahoo.com>
2023-11-13 02:49:15 -0800

    data access cleanup

--- das/data.go
+++ das/data.go
@@ -1,5 +1,16 @@
 package das
 
+const (
+	Herbivore     = "herbivore"
+	HerbivoreCode = "H"
+	Carnivore     = "carnivore"
+	CarnivoreCode = "C"
+	StatusDown    = "DOWN"
+	StatusActive  = "ACTIVE"
+
+	CageCapacity = 20
+)
+
 type Dinosaur struct {
 	ID      uint   `json:"id"`
 	Species string `json:"species"`
@@ -8,17 +19,20 @@ type Dinosaur struct {
 	Cage    uint   `json:"cage"`
 }
 
-const (
-	Herbivore     = "herbivore"
-	HerbivoreCode = "H"
-	Carnivore     = "carnivore"
-	CarnivoreCode = "C"
-)
-
 type Cage struct {
 	ID       int    `json:"id"`
 	Status   string `json:"status"`
 	Capacity int    `json:"capacity"`
 	Count    int    `json:"count"`
 	Kind     string `json:"kind"`
+}
+
+func ValidStatus(status string) bool {
+	switch status {
+	case StatusDown, StatusActive:
+		return true
+	default:
+		return false
+	}
+	return false
 }
